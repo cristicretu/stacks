0124d4893f75a9821b087610e8b084a9cfce26e0
Cristian Crețu <crisemcr@gmail.com>
2023-07-07 15:03:46 +0300

    Initial commit from Create Next App

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+
+import 'styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider attribute='class' disableTransitionOnChange>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
